10981f2f6e1a8babf08ef854a0d9c71b6436fd67
ValerianPerez <valerian.perez@gmail.com>
2018-08-11 18:33:38 +0200

    Rework on SlotController
    And remove of debug.log

--- Assets/Scripts/SlotController.cs
+++ Assets/Scripts/SlotController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SlotController : MonoBehaviour {
+public class SlotController : MonoBehaviour
+{
     /// <summary>
     /// The different states of a slot
     /// </summary>
     public enum SlotState
     {
         AVAILABLE,
-        OCCUPIED
+        OCCUPIED,
+        FROZEN,
+        BURRIED
     }
 
     /// <summary>
@@ -22,7 +25,7 @@ public class SlotController : MonoBehaviour {
     {
         CurrentState = SlotState.AVAILABLE;
 
-        GetComponent<Button>().onClick.AddListener(() => GameObject.Find("SceneManager").GetComponent<PlantController>().ClickOnSlot(transform.position));
+        GetComponent<Button>().onClick.AddListener(() => OnClick());
     }
 
     /// <summary>
@@ -30,12 +33,35 @@ public class SlotController : MonoBehaviour {
     /// </summary>
 	public void DisplayState()
     {
+        transform.Find("AvailableSprite").gameObject.SetActive(true);
         switch (CurrentState)
         {
             case SlotState.AVAILABLE:
                 transform.Find("AvailableSprite").gameObject.SetActive(true);
                 break;
             case SlotState.OCCUPIED:
+                break;
+            case SlotState.FROZEN:
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// The behaviour adding to the button
+    /// </summary>
+    private void OnClick()
+    {
+        switch (CurrentState)
+        {
+            case SlotState.AVAILABLE:
+                GameObject.Find("SceneManager").GetComponent<SceneManager>().ClickOnSlot(transform.position);
+                CurrentState = SlotState.OCCUPIED;
+                break;
+            case SlotState.OCCUPIED:
+                break;
+            case SlotState.FROZEN:
                 break;
             default:
                 break;
